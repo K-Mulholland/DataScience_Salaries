e548ccab3265d8844f87fad2f3e01d597f50773d
K-Mulholland <105945908+K-Mulholland@users.noreply.github.com>
2023-01-29 16:12:16 -0500

    updates

--- static/js/kmChoroplethLocation.js
+++ static/js/kmChoroplethLocation.js
@@ -15,6 +15,8 @@ const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
+
+
 var geojson;
 
 // Read in the CSV file
