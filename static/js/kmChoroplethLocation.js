24f9a0b6abb331e67f12bb442b5bfe4857a6882d
K-Mulholland <105945908+K-Mulholland@users.noreply.github.com>
2023-01-28 21:14:37 -0500

    updates

--- static/js/kmChoroplethLocation.js
+++ static/js/kmChoroplethLocation.js
@@ -1,35 +1,5 @@
-// adding path to csv 
+// adding path to geojson
 const source = './data/cc_Salary.geojson';
-
-
-
-
-
-// // Get the data with d3.
-// d3.json(geoData).then(function(data) {
-
-// 	// Create a new choropleth layer.
-// 	geojson = L.choropleth(data, {
-  
-// 	  // Define which property in the features to use.
-// 	  valueProperty: "DP03_16E",
-  
-// 	  // Set the color scale...use this if can't get chroma library below to work
-// 	  scale: ["#ffffb2", "#b10026"],
-  
-// 	  // The number of breaks in the step range
-// 	  steps: 10,
-  
-// 	  // q for quartile, e for equidistant, k for k-means
-// 	  mode: "q",
-// 	  style: {
-// 		// Border color
-// 		color: "#fff",
-// 		weight: 1,
-// 		fillOpacity: 0.8
-// 	  },
-
-
 
 
 // Create a map object
@@ -45,15 +15,20 @@ const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-var geoJson;
+var geojson;
 
 // Read in the CSV file
 d3.json(source).then(function(data) {
+
+
 	//processData(data);
   console.log("data_here", data)
+
+
 	// Create a new choropleth layer.
 	geojson = L.choropleth(data, {
   
+
 		// // Define which property in the features to use (is this tag correct?  check this)
 		valueProperty: "Avg Salary",
 		scale: ['#ffffb2', '#b10026'], // chroma.js scale - include as many as you like
@@ -69,21 +44,6 @@ d3.json(source).then(function(data) {
 	}
 }).addTo(map);})
 
-// // Plugin to create breaks in numerical values and assigns varying colors
 
-// L.choropleth(geojsonData, {
-// 	valueSalary: 'countries', // which value in countries to use
-// 	scale: ['white', 'red'], // chroma.js scale - include as many as you like
-// 	steps: 4, // number of breaks or steps in range
-// 	mode: 'q', // q for quantile, e for equidistant, k for k-means
-// 	style: {
-// 		color: '#fff', // border color
-// 		weight: 2,
-// 		fillOpacity: 0.8
-// 	},
-// 	onEachFeature: function(feature, layer) {
-// 		layer.bindPopup(feature.properties.value)
-// 	}
-// }).addTo(map)
 
 
